8d3ea9bcd47ec939d742e16deea17cfa5a05cda1
evelyn <eve.rodriguezval@gmail.com>
2025-06-17 23:13:18 +0100

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <img src="src/assets/planet-earth.png" alt="Planet earth photo" />
+            <h1>Wild Encounters.</h1>
+        </header>
+    )
+}
+
+export default Header;
